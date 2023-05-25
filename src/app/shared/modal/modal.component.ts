924ad0d4c9aa884ed50ac15667a29316a596b85e
Lucas Ferreira <ferreiralucas090@gmail.com>
2023-05-25 15:10:37 -0300

    fix: changing auth modal reference in the DOM

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, OnInit } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
@@ -6,10 +6,16 @@ import { ModalService } from 'src/app/services/modal.service';
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css']
 })
-export class ModalComponent {
+export class ModalComponent implements OnInit {
   @Input() modalID: string = ''
 
-  constructor(public modal: ModalService) {    
+  constructor(
+    public modal: ModalService,
+    public elementRef: ElementRef) {    
+  }
+
+  ngOnInit(): void {
+    document.body.appendChild(this.elementRef.nativeElement)
   }
 
   closeModal($event: Event) {
