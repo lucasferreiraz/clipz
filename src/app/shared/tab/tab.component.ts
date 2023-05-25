0261570ecbe61eba17af29834f7323d473df7174
Lucas Ferreira <ferreiralucas090@gmail.com>
2023-05-25 18:47:16 -0300

    feat: container tabs and tabs for modal

--- src/app/shared/tab/tab.component.ts
+++ src/app/shared/tab/tab.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-tab',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./tab.component.css']
 })
 export class TabComponent {
-
+  @Input() tabTitle: string = ''
+  @Input() active: boolean = false
 }
